5419de46d1a745ed6cc27bd334405838b6a8c562
Abdullah <Abdullahali8102@gmail.com>
2022-11-29 12:13:33 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This program will list the files in the Downloads directory, eventually moving files to their relevant destination directories.
+
+checkpoints:
+    1. list files in Downloads directory.
+
+    2. loop through files and use if/elif/else to apply actions to specific file types.
+
+    3. Move files out of the Downloads directory and into their revelant directories.
+
+In the added task you will record data such as filename, new location and date moved in a CSV file.
---
+++ src/dir_list.py
@@ -0,0 +1,13 @@
+# This program will list the files in the Downloads directory, eventually moving files to their relevant destination directories.
+
+# Steps:
+#     1. list files in Downloads directory.
+
+#     2. loop through files and use if/elif/else to apply actions to specific file types.
+
+#     3. Move files out of the Downloads directory and into their revelant directories.
+
+
+
+# Additional Task
+# In a csv file record filename, new locations of file and date + time for when the file was moved.
